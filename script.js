77872b5694d42e550430f7cc0c53c1d1c7bfbc81
jasonxshetty <jasonshetty0477@gmail.com>
2024-10-29 01:40:49 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Navigation Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
